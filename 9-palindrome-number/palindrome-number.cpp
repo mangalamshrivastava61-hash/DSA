483941aaae53d7038fa8e8a98e80dde5d6030dfb
mangalamshrivastava61-hash <mangalamshrivastava61@gmail.com>
2026-08-27 10:59:05 +0530

    Time: 4 ms (30.50%) | Memory: 8.7 MB (35.91%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0))
-            return false;
+        if (x < 0) return false;
 
-        int rev = 0;
+        int original = x;
+        long long rev = 0;
 
-        while (x > rev) {
-            rev = rev * 10 + x % 10;
+        while (x > 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
             x /= 10;
         }
 
-        return x == rev || x == rev / 10;
+        return original == rev;
     }
 };
